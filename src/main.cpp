47f4cc785bf8df29953c6bb87ac7302077c21945
Ryan Cobb <ryan.cobb@hey.com>
2022-02-14 22:05:38 -0800

    add encoder

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "M5CoreInk.h"
+#include "UNIT_ENCODER.h"
 #include "WiFi.h"
 #include "secrets.h"
+
+UNIT_ENCODER sensor;
 
 Ink_Sprite InkPageSprite(&M5.M5Ink);
 
@@ -15,18 +18,7 @@ void ButtonTest(char* str){
   delay(2000);
 }
 
-void setup() {
-  M5.begin(); //Initialize CoreInk.
-  if( !M5.M5Ink.isInit()){  //check if the initialization is successful.
-    Serial.printf("Ink Init faild");
-  }
-  M5.M5Ink.clear(); //Clear the screen.
-  delay(1000);
-  //creat ink Sprite.
-  if( InkPageSprite.creatSprite(0,0,200,200,true) != 0 ){
-    Serial.printf("Ink Sprite creat faild");
-  }
-
+void setupWifi() {
   while (status != WL_CONNECTED) {
     InkPageSprite.drawString(10, 10, "Attempting to connect to ");
     InkPageSprite.drawString(10, 25, ssid);
@@ -46,14 +38,52 @@ void setup() {
   InkPageSprite.pushSprite();
 }
 
+void setup() {
+  M5.begin(); //Initialize CoreInk.
+  if( !M5.M5Ink.isInit()){  //check if the initialization is successful.
+    Serial.printf("Ink Init faild");
+  }
+  M5.M5Ink.clear(); //Clear the screen.
+  delay(1000);
+  //creat ink Sprite.
+  if( InkPageSprite.creatSprite(0,0,200,200,true) != 0 ){
+    Serial.printf("Ink Sprite creat faild");
+  }
+
+  // setupWifi();
+
+  sensor.begin();
+}
+
+signed short int last_value = 0;
+
 void loop() {
-    if( M5.BtnUP.wasPressed()) ButtonTest("Btn UP Pressed");  //Scroll wheel up.
-    if( M5.BtnDOWN.wasPressed()) ButtonTest("Btn DOWN Pressed");  //Trackwheel scroll down.
-    if( M5.BtnMID.wasPressed()) ButtonTest("Btn MID Pressed");  //Dial down.
-    if( M5.BtnEXT.wasPressed()) ButtonTest("Btn EXT Pressed");  //Top button press.
-    if( M5.BtnPWR.wasPressed()){  //Right button press.
-        ButtonTest("Btn PWR Pressed");
-        M5.shutdown();  //Turn off the power, restart it, you need to wake up through the PWR button.
+  signed short int encoder_value = sensor.getEncoderValue();
+  bool btn_status = sensor.getButtonStatus();
+
+  if (last_value != encoder_value) {
+    if (last_value < encoder_value) {
+      sensor.setLEDColor(1, 0x000011);
+    } else {
+      sensor.setLEDColor(2, 0x111100);
     }
-    M5.update();  //Refresh device button.
+    last_value = encoder_value;
+  } else {
+    sensor.setLEDColor(0, 0x001100);
+  }
+  if (!btn_status) {
+    sensor.setLEDColor(0, 0xC800FF);
+  }
+  delay(20);
+
+
+    // if( M5.BtnUP.wasPressed()) ButtonTest("Btn UP Pressed");  //Scroll wheel up.
+    // if( M5.BtnDOWN.wasPressed()) ButtonTest("Btn DOWN Pressed");  //Trackwheel scroll down.
+    // if( M5.BtnMID.wasPressed()) ButtonTest("Btn MID Pressed");  //Dial down.
+    // if( M5.BtnEXT.wasPressed()) ButtonTest("Btn EXT Pressed");  //Top button press.
+    // if( M5.BtnPWR.wasPressed()){  //Right button press.
+    //     ButtonTest("Btn PWR Pressed");
+    //     M5.shutdown();  //Turn off the power, restart it, you need to wake up through the PWR button.
+    // }
+  // M5.update();  //Refresh device button.
 }
